30abba424f9fed5725b81fca74f2b44d731389d0
Ilya <gim78@yandex.ru>
2020-01-27 11:13:50 +0300

    result of changes

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,49 +3,42 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-
-        int inFirst = -1, inSecond = -1, res = 1;;
-        String inZnak = "p";
+        int first, second, res = 1;;
+        char sign;
 
         Scanner in = new Scanner(System.in);
-        Scanner in2 = new Scanner(System.in);
-        Scanner ch = new Scanner(System.in);
 
-        //Пока не введем положительное число будем повторять ввод (проверку на то, что число целое так и не сделал)
-        while(inFirst<=0){
+        do{
             System.out.print("enter the first figure: ");
-            inFirst = in.nextInt();
-        }
+            first = in.nextInt();
+        }while(first <=0 );
 
-        //Пока не введем нужный знак... 
-        while(!inZnak.equals("+") && !inZnak.equals("-") && !inZnak.equals("*")
-                && !inZnak.equals("/") && !inZnak.equals("%") && !inZnak.equals("^")){
+        do{
             System.out.print("enter a sign + - * / % or ^: ");
-            inZnak = ch.nextLine();
-        }
+             sign = in.nextChar();
+        }while(!sign.equals("+") && !sign.equals("-") && !sign.equals("*")
+                && !sign.equals("/") && !sign.equals("%") && !sign.equals("^"));
 
-        while (inSecond <= 0) {
+        do{
             System.out.print("enter the second figure: ");
-            inSecond = in2.nextInt();
-        }
+            second = in.nextInt();
+        }while (second <= 0);
 
-
-        if (inZnak.equals("+")) {
-            res = inFirst + inSecond;
-        } else if (inZnak.equals("-")) {
-            res = inFirst - inSecond;
-        } else if (inZnak.equals("*")) {
-            res = inFirst * inSecond;
-        } else if (inZnak.equals("/")) {
-            res = inFirst / inSecond;
-        } else if (inZnak.equals("%")) {
-            res = inFirst % inSecond;
+        if (sign.equals("+")) {
+            res = first + second;
+        } else if (sign.equals("-")) {
+            res = first - second;
+        } else if (sign.equals("*")) {
+            res = first * second;
+        } else if (sign.equals("/")) {
+            res = first / second;
+        } else if (sign.equals("%")) {
+            res = first % second;
         } else {
-            for (int i = 1; i <= inSecond; i++) {
-                res = res * inFirst;
+            for (int i = 1; i <= second; i++) {
+                res *= first;
             }
         }
-
         System.out.println("Result: " + res);
     }
 }
