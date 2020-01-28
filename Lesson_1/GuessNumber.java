34d90894fed42852f6445d7edd2732b40ce1d2ae
Ilya <gim78@yandex.ru>
2020-01-28 15:03:51 +0300

    HW1

---
+++ Lesson_1/GuessNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int hiddenNumber = 49, yourNumber;
+        do {
+            System.out.print("Введите Ваше число: ");
+            yourNumber = in.nextInt();
+            if (yourNumber > hiddenNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+            } else if (yourNumber < hiddenNumber)  {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+            }
+        } while(yourNumber != hiddenNumber);
+        System.out.println("Вы угадали!");
+    }
+}
