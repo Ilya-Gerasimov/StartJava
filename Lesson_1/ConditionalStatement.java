3c0cf3ed46b575d03aaf8c023a48bb1577ababd9
Ilya <gim78@yandex.ru>
2020-01-28 09:16:46 +0300

    HW1-1

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,33 +1,34 @@
-public class ConditionalStatement {	
+public class ConditionalStatement {
     public static void main(String[] args) {
-    	int age = 49, height = 172;
-    	boolean male = true;
-    	char firstLetterName = 'I';
+        int age = 49;
 
-    	if (age > 20) {
-    		System.out.println("Вы уже можете посещать бар!");
-    	}
+        if (age > 20) {
+            System.out.println("Вы уже можете посещать бар!");
+        }
 
-    	if (male) {
-    		System.out.println("Вы скорее всего мужчина!");
-    	}
+        boolean isMale = true;
+        if (isMale) {
+            System.out.println("Вы скорее всего мужчина!");
+        }
 
-    	if (!male) {
-    		System.out.println("Вы скорее всего женщина!");
-    	}
+        if (!isMale) {
+            System.out.println("Вы скорее всего женщина!");
+        }
 
-    	if (height < 180) {
-    		System.out.println("Вы ниже 180 см.");
-    	} else {
-    		System.out.println("Вы выше или равны 180 см.");
-    	}
+        int height = 172;
+        if (height < 180) {
+            System.out.println("Вы ниже 180 см.");
+        } else {
+            System.out.println("Вы выше или равны 180 см.");
+        }
 
-    	if (firstLetterName == 'M') {
-    		System.out.println("Ваше имя начинается на букву М");
-    	} else if (firstLetterName == 'I') {
-    		System.out.println("Ваше имя начинается на букву I");
-    	} else {
-    		System.out.println("Ваше имя начинается на другую букву");
-    	}
+        char firstLetterName = 'I';
+        if (firstLetterName == 'M') {
+            System.out.println("Ваше имя начинается на букву М");
+        } else if (firstLetterName == 'I') {
+            System.out.println("Ваше имя начинается на букву I");
+        } else {
+            System.out.println("Ваше имя начинается на другую букву");
+        }
     }
 }
