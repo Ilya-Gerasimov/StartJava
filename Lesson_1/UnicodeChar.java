f679c95eaf4147fdcda24105ac16fe6fde11ff8c
Ilya <gim78@yandex.ru>
2020-01-29 08:36:54 +0300

    HW1

--- Lesson_1/UnicodeChar.java
+++
@@ -1,7 +0,0 @@
-public class UnicodeChar {
-    public static void main(String[] args) {
-        for (int i = 33; i <= 126; i++) {
-            System.out.println(Character.valueOf((char) i));
-        }
-    }
-}
