56e8e90eb539c36ff27d172c25d57693e9c8e1af
Ilya <gim78@yandex.ru>
2020-01-29 12:55:30 +0300

    HW1-2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int hiddenNumber = 49, yourNumber;
+        int hiddenNumber = 49, playerNumber;
         do {
             System.out.print("Введите Ваше число: ");
-            yourNumber = in.nextInt();
-            if (yourNumber > hiddenNumber) {
+            playerNumber = in.nextInt();
+            if (playerNumber > hiddenNumber) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
-            } else if (yourNumber < hiddenNumber) {
+            } else if (playerNumber < hiddenNumber) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
             }
-        } while(yourNumber != hiddenNumber);
+        } while(playerNumber != hiddenNumber);
         System.out.println("Вы угадали!");
     }
 }
