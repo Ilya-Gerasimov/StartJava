33fb00e46e897ed5af559a55389849207cc479f7
Ilya <gim78@yandex.ru>
2020-01-30 09:57:41 +0300

    HW2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name = "Ilya";
+    boolean isMale = true;
+    float height = 170.2f, weight = 80.1f;
+    int age = 49;
+
+    void move() {
+        System.out.println("Вы начали движение");
+    }
+
+    void run() {
+        System.out.println("Вы побежали");
+    }
+
+    boolean sit() {
+        System.out.println("Вы сели");
+        return true;
+    }
+
+    void speak() {
+        System.out.println("Вы говорите");
+    }
+
+    void teachJava() {
+        System.out.println("Вы начали изучать Java");
+    }
+}
